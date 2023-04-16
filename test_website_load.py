4fe4586633b81317a52562da50e4c14f3d6ea5b6
amit099 <91498106+amit099@users.noreply.github.com>
2023-04-16 19:34:02 +0530

    Update test_website_load.py

--- test_website_load.py
+++ test_website_load.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_atg_website():
-    url = 'https://atg.world/'
+    url = 'https://122#%@#%@$@$@%ghgfhfhghffh!$@#@#/'
     print(f"Trying to load {url}")
     response = requests.get(url)
     assert response.status_code == 200, f'Failed to load {url}'
