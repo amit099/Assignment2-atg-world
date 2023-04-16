5eb6255d1a0482ad6d3b18799591fd2639306438
amit099 <91498106+amit099@users.noreply.github.com>
2023-04-16 19:12:37 +0530

    Create test_website_load.py

---
+++ test_website_load.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import unittest
+
+class TestWebsiteLoad(unittest.TestCase):
+    def test_load_website(self):
+        driver = webdriver.Chrome()
+        driver.get("https://atg.world/")
+        self.assertIn("ATG - Access To Global", driver.title)
+        driver.quit()
