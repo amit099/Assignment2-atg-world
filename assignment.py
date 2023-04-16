caadec16cfecb77d9b32ce330508522490664927
amit099 <91498106+amit099@users.noreply.github.com>
2023-04-17 04:33:46 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 service = Service()
 options= Options()
-#driver_service = Service(executable_path="/usr/bin/chromedriver")
+driver_service = Service(executable_path="/usr/bin/chromedriver")
 #service.headless = True
 
 class TestWebsiteLoad(unittest.TestCase):
@@ -16,7 +16,7 @@ class TestWebsiteLoad(unittest.TestCase):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome('/usr/bin/chromedriver', options=options) # Use any browser driver of your choice.
+        driver = webdriver.Chrome(service=driver_service, options=options) # Use any browser driver of your choice.
         driver.get("https://atg.world/")
         print(driver.title)
         driver.quit()
