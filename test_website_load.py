dda4230b6d7e3fe80a14def5dc1965c613e3b262
amit099 <91498106+amit099@users.noreply.github.com>
2023-04-16 19:21:50 +0530

    Update test_website_load.py

--- test_website_load.py
+++ test_website_load.py
@@ -4,6 +4,6 @@ import unittest
 class TestWebsiteLoad(unittest.TestCase):
     def test_load_website(self):
         driver = webdriver.Chrome()
-        driver.get("https://ydhyehg.world/")
+        driver.get("https://123")
         self.assertIn("ATG - Access To Global", driver.title)
         driver.quit()
